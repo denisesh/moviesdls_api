297ecfa3a54b243fce7943da54f685d9971b8594
denisesh <denisesh@gmail.com>
2015-01-26 20:40:25 -0800

    searches work

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,16 @@ class MoviesController < ApplicationController
   def all
     @movie = Movie.all
     render json: @movie
+  end
 
+  def search
+    if params.has_key?("t")
+      @movie = Movie.where("title LIKE ?", "#{params[:title]}%")
+    end
+
+    if params.has_key?("y")
+      @movie = Movie.where(year: params["y"])
+    end
+    render json: @movie
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'movies#all'
 
   get '/all' => 'movies#all'
+  get "/search" => "movies#search"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
